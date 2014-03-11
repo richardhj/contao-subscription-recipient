f15ae24e0a51b9e37700badf02ba9edd9a7dc60b
Tristan Lins <tristan.lins@bit3.de>
2014-03-11 15:53:42 +0100

    Add module description.

---
+++ module/languages/en/modules.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-subscription-recipient
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Module
+ */
+$GLOBALS['TL_LANG']['MOD']['avisota-subscription-recipient'] = array(
+	'Avisota - Subscription for recipients',
+	'Recipient management and subscription for Avisota.'
+);
