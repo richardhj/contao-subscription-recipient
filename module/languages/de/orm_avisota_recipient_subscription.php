ca17ff93946fa7b09e25039e5189f4f3a337688e
Avibot <avibot@avisota.org>
2014-03-26 18:29:04 +0100

    Auto-Update translations from transifex

---
+++ module/languages/de/orm_avisota_recipient_subscription.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-03-25T14:15:16+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['orm_avisota_recipient_subscription']['confirmed']['0']                    = 'Bestätigt';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_subscription']['confirmed']['1']                    = 'Dieses Abonnement wurde bestätigt.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_subscription']['confirmedAt']['0']                  = 'Bestätigt am';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_subscription']['createdAt']['0']                    = 'Erstellt am';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_subscription']['list']['0']                         = 'Abonnierte Liste';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_subscription']['new']['0']                          = 'Neues Abonnement';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_subscription']['recipient']['0']                    = 'Abonnent';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_subscription']['subscription_list']['mailing_list'] = 'Mailinglisten';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_subscription']['updatedAt']['0']                    = 'Zuletzt bearbeitet am';
+
