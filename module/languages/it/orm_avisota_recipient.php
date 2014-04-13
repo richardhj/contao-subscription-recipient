56f09d8d6e40107ede0ce95e784c2a8e077e66cd
Avibot <avibot@avisota.org>
2014-04-13 04:02:29 +0200

    Auto-Update translations from transifex

---
+++ module/languages/it/orm_avisota_recipient.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/it/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-04-07T04:01:30+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['confirmed']['0']          = 'Confermato';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['email']['0']              = 'Email';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['export']['0']             = 'Esporta CSV';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['import']['0']             = 'Importa CSV';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['personals_legend']        = 'Personali';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['recipient_legend']        = 'Destinatario';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['remove']['0']             = 'Elimina CSV';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['subscriptionAction']['0'] = 'Attivazione';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['subscription_legend']     = 'Sottoscrizione';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['title']['0']              = 'Titolo';
+
