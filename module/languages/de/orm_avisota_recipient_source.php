fc054570d18ba3c5efabca8e8943507fde8b185e
Avibot <avibot@avisota.org>
2014-05-26 04:00:42 +0200

    Auto-Update translations from transifex

---
+++ module/languages/de/orm_avisota_recipient_source.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-03-25T14:15:15+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['recipients']['0']                       = 'Empfänger auswählen &hellip;';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['recipients']['1']                       = 'Bitte wählen Sie, wie Empfänger ausgewählt werden.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['recipientsDetails']['0']                = 'Details zusammenführen aus &hellip;';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['recipientsDetails']['1']                = 'Bitte wählen Sie aus, von woher die Details zusammengeführt werden sollen.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['recipientsManageSubscriptionPage']['0'] = 'Verwaltungsseite';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['recipientsManageSubscriptionPage']['1'] = 'Bitte wählen Sie die Seite aus, auf der das Abonnement verwalten wird.';
+
