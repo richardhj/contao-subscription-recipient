6227e2f2f41d8bdbf01322be5bd901850aa13d86
Avibot <avibot@avisota.org>
2014-07-05 04:00:58 +0200

    Auto-Update translations from transifex

--- module/languages/rm/fe_avisota_subscription.php
+++ module/languages/rm/fe_avisota_subscription.php
@@ -11,13 +11,14 @@
  * last-updated: 2014-03-25T14:15:11+01:00
  */
 
-
 $GLOBALS['TL_LANG']['fe_avisota_subscription']['allreadySubscribed']    = 'Grazia fitg, ti has dentant gia abunà nos newsletter.';
 $GLOBALS['TL_LANG']['fe_avisota_subscription']['confirm']               = 'Confermar abunaments';
+$GLOBALS['TL_LANG']['fe_avisota_subscription']['manage_subscription']   = 'Administrat tes abunament';
 $GLOBALS['TL_LANG']['fe_avisota_subscription']['notSubscribed']         = 'Ti n\'has betg abunà nos newsletter';
 $GLOBALS['TL_LANG']['fe_avisota_subscription']['subscribe']             = 'Abunar';
 $GLOBALS['TL_LANG']['fe_avisota_subscription']['subscribeConfirmation'] = 'Tes abunament è vegnì activà cun success.';
 $GLOBALS['TL_LANG']['fe_avisota_subscription']['subscribed']            = 'Grazia fitg, ti es ussa abunnent. Ti has retschiert in e-mail da confermaziun.';
 $GLOBALS['TL_LANG']['fe_avisota_subscription']['unsubscribe']           = 'De-abunar';
+$GLOBALS['TL_LANG']['fe_avisota_subscription']['unsubscribe_direct']    = 'De-abunar';
 $GLOBALS['TL_LANG']['fe_avisota_subscription']['unsubscribed']          = 'Ti has ussa de-abunà nos newsletter';
 
