cbd53c585b809af1304da405038ed1603567df20
Sven Baumann <baumann.sv@gmail.com>
2015-07-28 22:28:20 +0200

    add author

--- src/Avisota/Contao/SubscriptionRecipient/EventsSubscriber.php
+++ src/Avisota/Contao/SubscriptionRecipient/EventsSubscriber.php
@@ -8,6 +8,7 @@
  *
  * @copyright  bit3 UG 2013
  * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
  * @package    avisota/contao-subscription-recipient
  * @license    LGPL-3.0+
  * @filesource
